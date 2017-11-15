66deeab277bc63fbd9feb5f8580a533873af9d9b
Reggie Cooper <reggie@Reggies-MacBook-Pro.local>
2017-11-15 15:04:49 -0800

    Initial deploy

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'balls'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
+});
