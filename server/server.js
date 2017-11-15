2445d2d1d2a1f3bdfee7178fd230d00a1ad8a32c
Reggie Cooper <reggie@Reggies-MacBook-Pro.local>
2017-11-15 15:16:35 -0800

    fixed

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '../views/partials');
 
 const port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '../public'));
 
 app.set('view engine', 'hbs');
 
